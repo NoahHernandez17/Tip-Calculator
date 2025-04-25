64f2beb1808ef6f23680f819a0bd824691abdac3
NoahHernandez17 <145629045+NoahHernandez17@users.noreply.github.com>
2025-04-25 14:46:29 -0700

    Update TIp-Calculator.py

--- TIp-Calculator.py
+++ TIp-Calculator.py
@@ -1,6 +1,6 @@
 print("Welcome to the tip calculator!")
 bill = float(input("What was the total bill? $"))
-tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+tip = int(input("What percentage tip would you like to give? 10%, 12%, or 15% "))
 people = int(input("How many people to split the bill? "))
 if tip == 15:
     total = (bill / people) * 1.15
